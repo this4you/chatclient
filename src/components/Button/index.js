4ac25447b4d0dce718108e312102022ca776922a
this4you <me_vlad@ukr.net>
2021-03-03 22:00:48 +0200

    test

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {Button as BaseButton} from "antd";
+
 import  classNames from 'classnames';
 import './Button.scss';
 const Button = (props) => {
--- src/utils/generateAvatarFromHash.js
+++ src/utils/generateAvatarFromHash.js
@@ -1,22 +1,24 @@
 import tinycolor from 'tinycolor2';
-function  getCorrectIndex(number) {
-    return  number > 255 ? 255 : number < 0 ? 0 : number;
+
+function getCorrectIndex(number) {
+    return number > 255 ? 255 : number < 0 ? 0 : number;
 }
+
 function generateAvatarFromHash(hash) {
     const [r, g, b] = hash
-        .substr(0, 3)
+        .substr(5, 3)
         .split("")
         .map(char => getCorrectIndex(char.charCodeAt(0)));
-
     return {
-        color: tinycolor({ r, g, b })
+        color: tinycolor({r, g, b})
             .lighten(10)
             .saturate(10)
             .toHexString(),
-        colorLighten: tinycolor({ r, g, b })
+        colorLighten: tinycolor({r, g, b})
             .lighten(30)
             .saturate(30)
             .toHexString()
     };
 }
+
 export default generateAvatarFromHash;
